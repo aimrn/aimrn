5ea925def0de18a757587c9c25f20cd7e5616e30
AIMRN <amimanicrightnow@gmail.com>
2020-04-28 16:30:25 -0700

    Add to index page and create faq list.

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
+    # FIX: move this data out to a separate file so the app.py is less cluttered
     raw_questions = [
         "",  # We will waste the zeroth index 
         "My mind has never been sharper.",
@@ -31,4 +32,55 @@ def index():
     questions = [{"num": str(index), "question": element} for index, element in enumerate(raw_questions)]
     questions = questions[1:]  # Waste the zeroth index
 
+    raw_second_questions = [
+        # List should be increasing craziness/pathologicalness. It jumps down once you say yes to maybe three of them or something
+        # Otherwise they keep appearing. Also you stop having the option of yes/no. it becoems just yes, then it becomes just the radio button
+        "",  # Waste zeroth
+        "Can you fix everything?",
+        "Is everything amazing?",
+        "Do you have a solution to all your problems?",
+        "Is today the day your going to clean everything?",
+        "And start running every day?",
+        "And wake up early?",
+        "And sleep by 10?",
+        "Have you spent more than $100 today?",
+        'Is your finger hovering over the bid button in eBay for a "...limited edition, special release, holo only first edition pokemon card complete set...',
+        "Hold up, you have more than 20 tabs open right now?",
+        "Did you sleep last night?",
+        "WAIT, what do you mean /which night/ am I talking about? THATS EXACTLY THE PROBLEM 0_0",
+        "YOU HAVEN'T SLEPT IN HOW MANY DAYS??",
+        "IS THAT A PAINTBRUSH IN YOUR HAND??!!1",
+        
+    ]
+
     return render_template("index.html", questions=questions)
+
+def faq():
+    raq_qa = [
+        {
+            # FIX put a reverence number thing on the main page that links to here
+            "question": "Do you have any data to back that up? I'm not manic.",
+            "answer": "But are you though? :D In all seriousness yes I don't have any data to back it up. It's a joke. But I would surprised if there /wasn't/ a positive selection bias.",
+        },
+        {
+            "question": "the question text",
+            "answer": "the answer text",
+        },
+        {
+            "question": "the question text",
+            "answer": "the answer text",
+        },
+
+    ]
+
+
+# TODO ROUTES
+# about/why page
+# contact page
+# faq
+# index for the paranoid
+# yes(next page)
+# no(next page)
+    # maybe no just redirects to yes?
+    # maybe with a funny something
+    # dunno
