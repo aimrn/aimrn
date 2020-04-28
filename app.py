0cf323c6fa74e78f136af4b39c8ca3786c923638
AIMRN <amimanicrightnow@gmail.com>
2020-04-27 22:29:13 -0700

    Make page template.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
-    return "TODO"
+    return render_template("index.html")
